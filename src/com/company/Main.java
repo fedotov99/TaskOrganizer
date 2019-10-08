b5a22f6e64ac1e15295b3e6b66cadcb7c984b7ce
Vlad Fedotov <fedotovvlad@list.ru>
2019-10-09 02:20:31 +0300

    Sample which represents the bug

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Task t  = new Task("Write email to customer");
+        t.setPriority(PriorityType.URGENT);
+        t.printTaskInfo(); // 1 Write email to customer URGENT false
+
+        ManagerUser Ann = new ManagerUser("Ann");
+        SubordinateUser Paul = new SubordinateUser("Paul", Ann, 0, PositionType.Junior);
+        System.out.println(Ann.getUserID());
+        System.out.println(Paul.getUserID());
+        System.out.println(Ann.getName());
+        System.out.println(Paul.getName());
+        Ann.addTask(t); // bug
+       // Ann.assignTaskToSubordinate(t, Paul);
+       // Paul.completeTask(t.getTaskID(), "I made this task, Ann, check it please!");
     }
 }
