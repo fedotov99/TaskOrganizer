f66eb3f081612c9b3281303e26d7ca82e722d077
Vlad Fedotov <fedotovvlad@list.ru>
2019-11-15 18:33:59 +0300

    ManagerController updated

--- src/main/java/com/company/controller/ManagerController.java
+++ src/main/java/com/company/controller/ManagerController.java
@@ -23,9 +23,9 @@ public class ManagerController {
 
     // create
     @PostMapping("/manager")
-    public String createManagerUser(@RequestParam String name) {
+    public ManagerUser createManagerUser(@RequestParam String name) {
         ManagerUser mu = managerTasksService.createManagerUser(name);
-        return mu.toString();
+        return mu;
     }
 
     // retrieve
@@ -34,23 +34,25 @@ public class ManagerController {
         return managerTasksService.getByUserID(id);
     }
 
+/* // TODO: solve ambiguous mapping
     @GetMapping("/manager/{name}")
     public ManagerUser getManagerUserByName(@PathVariable String name) {
         return managerTasksService.getByName(name);
     }
+*/
 
     @GetMapping("/manager")
     public List<ManagerUser> getAllManagers(){
         return managerTasksService.getAll();
     }
 
-    @GetMapping("manager/{id}/")
+    //@GetMapping("manager/{id}/")
 
     // update
     @PutMapping("/manager/{id}")
-    public String updateManagerUser(@PathVariable String id, @RequestParam String name, @RequestParam Map<String, SubordinateUser> subordinateList, @RequestParam Map<String, Task> uncheckedTasksList) {
-        ManagerUser mu = managerTasksService.updateManagerUser(id, name, subordinateList, uncheckedTasksList);
-        return mu.toString();
+    public ManagerUser updateManagerUser(@PathVariable String id, @RequestBody ManagerUser manager) {
+        ManagerUser mu = managerTasksService.updateManagerUser(id, manager.getName(), manager.getSubordinateList(), manager.getUncheckedTasksList());
+        return mu;
     }
 
     // delete
