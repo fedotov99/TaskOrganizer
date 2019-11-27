9138d519aabb32553ecb288903d40808af408c23
Vlad Fedotov <fedotovvlad@list.ru>
2019-11-28 01:17:09 +0300

    added email and password fields to user

--- src/main/java/com/company/model/User.java
+++ src/main/java/com/company/model/User.java
@@ -9,6 +9,8 @@ public abstract class User {
     private String userID;
     private static int counter = 1;
     private String name;
+    private String email; // TODO: make unique
+    private String password;
     private Map<String, Task> localUserTaskList = new HashMap<String, Task>();
 
     public User() {
@@ -32,6 +34,22 @@ public abstract class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Map<String, Task> getLocalUserTaskList() {
