27633483d9f9a01e5904aad56faeced346ac10d0
Vlad Fedotov <fedotovvlad@list.ru>
2019-11-29 12:45:22 +0300

    login controller added

---
+++ src/main/java/com/company/controller/LoginController.java
@@ -0,0 +1,45 @@
+package com.company.controller;
+
+import com.company.model.*;
+import com.company.service.ManagerTasksService;
+import com.company.service.SubordinateTasksService;
+import com.company.service.UserTasksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@RestController
+public class LoginController {
+    @Autowired
+    private UserTasksService userTasksService;
+    @Autowired
+    private ManagerTasksService managerTasksService;
+    @Autowired
+    private SubordinateTasksService subordinateTasksService;
+
+    @PostMapping("/login")
+    public AuthRespond authenticate(@RequestBody AuthBody authBody) {
+        String role = "";
+        UserDetails userDetails = userTasksService.loadUserByUsername(authBody.getEmail());
+        User user = subordinateTasksService.getByEmail(authBody.getEmail());
+        if (user == null) {
+            user = managerTasksService.getByEmail(authBody.getEmail());
+            role = ManagerUser.getRole();
+        }
+        else {
+            role = SubordinateUser.getRole();
+        }
+
+        if (user.getPassword().equals(authBody.getPassword())) {
+            return new AuthRespond(user.getUserID(), role, true);
+        }
+        else
+            return new AuthRespond("", "", false);
+    }
+}
