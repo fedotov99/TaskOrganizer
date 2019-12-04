6f76da680c9d7e0bfa6160abb8df34d6d7c55e18
Vlad Fedotov <fedotovvlad@list.ru>
2019-12-05 01:00:47 +0300

    cors fixed

--- src/main/java/com/company/SecurityConfig.java
+++ src/main/java/com/company/SecurityConfig.java
@@ -30,13 +30,15 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
         auth
-                .userDetailsService(userDetailsService);
-                //.passwordEncoder(encoder());
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/login", "/create/manager", "/create/subordinate", "/util/manager").permitAll()
